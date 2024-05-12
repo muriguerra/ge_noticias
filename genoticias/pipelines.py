3c26f976bde1677de20dbe6de4fbec7d7355b855
Murilo Guerra <89086669+muriguerra@users.noreply.github.com>
2024-05-12 09:46:52 -0300

    adição comentários e mudança nome tabela final

--- genoticias/pipelines.py
+++ genoticias/pipelines.py
@@ -16,15 +16,17 @@ import pandas_gbq
 class GenoticiasPipeline:
     dicio = []
 
-    # This method is called when the spider is opened.
+    # Este método é chamado quando a spider é aberta.
     # https://docs.scrapy.org/en/latest/topics/item-pipeline.html
     def open_spider(self, spider):
 
 
         self.dicio_lista = []
 
+    # Este método é chamado para processar cada item coletado pela spider
     def process_item(self, item, spider):
         print('Esses são os itens', item)
+        # Cria um dicionário com os dados do item
         dicio = {
                 'autor': item['autor'],
                 'titulo': item['titulo'],
@@ -40,24 +42,22 @@ class GenoticiasPipeline:
 
         return item
 
+    # Este método é chamado quando a spider é fechada
     def close_spider(self, spider):
-        # Convert the list of data into a pandas DataFrame
+        # Converte a lista de dicionários em um DataFrame pandas
         df = pd.DataFrame(columns=['autor', 'titulo', 'subtitulo', 'link', 'publicacao'])
         df = pd.DataFrame(self.dicio_lista)
         print('dataframe: ', df.head(5))
 
-        # BigQuery configuration
+        # Configuração do BigQuery
         project_id = 'noticias-scrapy'
-        table_id = 'gesports.teste'
-
-
+        table_id = 'gesports.noticias-gesports'
         key_path = "C:/Users/muril/OneDrive/Documentos/prog/gesports_key.json"
-
         credentials = service_account.Credentials.from_service_account_file(
             key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
         )
 
-        # Load the DataFrame to BigQuery
+        # Carrega o DataFrame para o BigQuery
         pandas_gbq.to_gbq(df, table_id, project_id, if_exists='replace', credentials=credentials)
 
 
--- genoticias/spiders/ge.py
+++ genoticias/spiders/ge.py
@@ -15,13 +15,16 @@ class GeSpider(scrapy.Spider):
     pagina = 2
 
     def parse(self, response, **kwargs):
+        # Extrai os links de todas as notícias na página inicial
         links_noticias = response.css("a.feed-post-link::Attr('href')").getall()
+        # Itera sobre os links das notícias e faz uma requisição para cada uma delas
         for link in links_noticias:
             yield scrapy.Request(link, callback=self.parse_noticia, cb_kwargs={
                 'link': link
             })
-
+        # Constrói a URL da próxima página com base no número da página
         proxima_pagina = 'https://ge.globo.com/esports/index/feed/pagina-' + str(GeSpider.pagina) + '.ghtml'
+        # Verifica se a próxima página está dentro do limite (neste caso, 10 páginas)
         if GeSpider.pagina <= 10:
             GeSpider.pagina += 1
             yield scrapy.Request(proxima_pagina, callback=self.parse)
@@ -30,7 +33,7 @@ class GeSpider(scrapy.Spider):
 
     def parse_noticia(self, response, **kwargs):
         items = GenoticiasItem()
-
+        # Extrai as informações da notícia e atribui aos campos do item
         items['autor'] = response.css('.content-publication-data__from::text').get()
         items['titulo'] = response.css('.content-head__title::text').get()
         items['subtitulo'] = response.css('.content-head__subtitle::text').get()
